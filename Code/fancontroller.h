abf98b703f5d32c3520f8130b3d67c18c5561e63
Ross Cathcart <rdcscotland@outlook.com>
2024-01-29 19:36:51 +0000

    started writing driver for fan pwm

---
+++ Code/fancontroller.h
@@ -0,0 +1,26 @@
+#ifndef FANCONTROLLER_H_
+#define FANCONTROLLER_H_
+
+/******************************************
+ * INCLUDES
+ ******************************************/
+#include <msp430.h>
+
+/******************************************
+ * DEFINES
+ ******************************************/
+#define FAN_PIN BIT3 // P8.3
+#define FAN_PWM_PERIOD 639// 20kHz
+#define FAN_PWM_MIN_DUTY 125 // Placeholder! Needs experimental verification
+
+/******************************************
+ * API FUNCTION DECLARATIONS
+ ******************************************/
+
+/*
+ * fan_init - Initializes GPIO P8.3 and configures Timer A1 for PWM
+ */
+void fan_init();
+
+
+#endif /*FANCONTROLLER_H_
