be5eceedf739a889a530ba1fc5a69e36715caf63
Ross Cathcart <rdcscotland@outlook.com>
2024-02-09 10:06:08 +0000

    decoder finished

--- Software Testing/servo_testing/decoder.c
+++ Software Testing/servo_testing/decoder.c
@@ -3,20 +3,36 @@
 void decoder_pins_init()
 {
     // Configure select output pins
-    P1DIR |= SEL0;
-    P1DIR |= SEL1;
-    P1OUT &= ~SEL0;
-    P1DIR &= ~SEL1;
+    P1DIR |= SELA;
+    P1DIR |= SELB;
+    P1OUT &= ~SELA;
+    P1DIR &= ~SELB;
 }
 
 void decoder_set_led(int led_number)
 {
     switch(led_number)
     {
-    case 0:
-        // finish, set corresponding sela/selb high or low based on TT
-
-
+    case 0: // Sel = "00" light LED Y0
+        P1OUT &= ~SELB;
+        P1OUT &= ~SELA;
+        break;
+    case 1: // Sel = "01" light LED Y1
+        P1OUT &- ~SELB;
+        P1OUT |= SELA;
+        break;
+    case 2: // Sel = "10" light LED Y2
+        P1OUT |= SELB;
+        P1OUT &= ~SELA;
+        break;
+    case 3: // Sel = "11" light LED Y3
+        P1OUT |= SELB;
+        P1OUT |= SELA;
+        break;
+    case default: // Should never occur but light Y0 in this event
+        P1OUT &= ~SELB;
+        P1OUT &= ~SELA;
+        break;
     }
 
 }
--- Software Testing/servo_testing/decoder.h
+++ Software Testing/servo_testing/decoder.h
@@ -5,8 +5,8 @@
 // "10" => Output Y2 low
 // "11" => Output Y3 low
 
-#ifndef FANCONTROLLER_H_
-#define FANCONTROLLER_H_
+#ifndef DECODER_H_
+#define DECODER_H_
 
 /******************************************
  * INCLUDES
@@ -25,8 +25,43 @@
  * API FUNCTION DECLARATIONS
  ******************************************/
 
+
+/*
+ * decoder_pins_init - Initialises the GPIO pins for the 2-to-4 decoder selection lines.
+ *
+ * This function configures the GPIO pins corresponding to the selection lines (SelA and SelB) of the 2-to-4 decoder.
+ * It sets these pins as outputs and initialises their initial states.
+ *
+ * @param None
+ *
+ * @return None
+ *
+ * Note: This function assumes that the selection lines SelA and SelB are connected to GPIO pins (such as P1OUT)
+ * and need to be configured prior to use.
+ *
+ * Example usage:
+ *      Call this function once at the beginning of the program to initialise the decoder pins.
+ */
 void decoder_pins_init();
 
+/*
+ * decoder_set_led - Sets the LED based on the input LED number using a 2-to-4 decoder logic.
+ *
+ * This function sets the LED corresponding to the input LED number using a 2-to-4 decoder logic. The LED number
+ * is used to determine the selection lines (SelA and SelB) required to activate the desired LED output.
+ *
+ * @param led_number An integer specifying the LED number to be lit up (0 to 3).
+ *
+ * @return None
+ *
+ * Note: This function assumes that the selection lines SelA and SelB are connected to GPIO pins (such as P1OUT)
+ * and have been configured as outputs prior to its invocation.
+ *
+ * Example usage:
+ *      To light up LED Y2:
+ *          decoder_set_led(2);
+ */
 void decoder_set_led(int led_number);
+
 
 #endif
