781e9bdf764ede0b11c50477f7bf35088bf7878a
RDC23 <112988260+RDC23@users.noreply.github.com>
2024-02-15 15:19:05 +0000

    Update fancontroller.c
    fixed fan pwm timer setup in fan_init() - it wasn't setting the TA0CTL or TACCR0 before so PWM wasnt working

--- Software Testing/temp_control_fan/fancontroller.c
+++ Software Testing/temp_control_fan/fancontroller.c
@@ -10,7 +10,9 @@ void fan_init()
 
     // Setup the timer for PWM (uses CCR2 to adjust the duty cycle)
     TA0CCTL2 |= OUTMOD_7; // PWM reset/set
+    TA0CCR0 |= FAN_PWM_MAX_DUTY;
     TA0CCR2 |= FAN_PWM_MAX_DUTY; // Startup at max speed to provide high initial torque
+    TA0CTL |= TASSEL_1 | MC_1 | ID_0 | TACLR; // ACLK, up to CCR0, divider = 1
 }
 
 void fan_torque_boost()
