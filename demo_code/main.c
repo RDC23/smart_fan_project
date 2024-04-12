0be686a3ba88108cc130a7bf3cb1444f8564481f
Ross Cathcart (Student) <ross.cathcart.2021@uni.strath.ac.uk>
2024-04-12 10:02:45 +0100

    modified main.c to only use PIR for detection

--- demo_code/main.c
+++ demo_code/main.c
@@ -88,6 +88,18 @@ bool movement_event(int cdist)
     }
 }
 
+bool movement_event_pir_only()
+{
+    if (activated_direction != cur_servo_ang)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
 void cycle_fan_mode()
 {
     switch (current_mode)
@@ -115,7 +127,7 @@ int main(void)
     
     init_thermistor();
     pir_init();
-    ultrasonic_setup_pins();
+    // ultrasonic_setup_pins();
     init_GPIO();
     fan_init();
     servo_init();
@@ -230,16 +242,22 @@ int main(void)
             case TRACK:
 
                 // Fire ultrasonic pulse to provide current distance measurement
-                ultrasonic_fire_pulse();
-                current_dist = ultrasonic_get_distance();
+
+                // ultrasonic_fire_pulse();
+                // current_dist = ultrasonic_get_distance();
 
                 // If a 'movement event' is detected, turn the servo to face the PIR
-                if (movement_event(current_dist))
+                // if (movement_event(current_dist))
+                // {
+                //     servo_cycle_gradual(activated_direction);
+                //     // Update the last distance with the target position in the new servo frame
+                //     ultrasonic_fire_pulse();
+                //     last_measured_distance = ultrasonic_get_distance();
+                // }
+
+                if movement_event_pir_only()
                 {
                     servo_cycle_gradual(activated_direction);
-                    // Update the last distance with the target position in the new servo frame
-                    ultrasonic_fire_pulse();
-                    last_measured_distance = ultrasonic_get_distance();
                 }
                 break;
 
